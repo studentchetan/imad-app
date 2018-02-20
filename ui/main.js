cd9557d45cc9ad71b56e65dd4578217928f4dd5e
studentchetan <chetankar65@gmail.com>
2018-02-20 17:35:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,21 @@ request.onreadystatechange = function(){
 request.open('GET','http://chetankar65.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+    //make request to server and send name
+    
+    //capture a list of names and render it as list
+    var names = ['Name1','Name2','Name3','Name4'];
+    var list = '';
+    for (var i=0;i < names.length; i++){
+        list += '<li>' + names[i] + '<li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+    
+};
