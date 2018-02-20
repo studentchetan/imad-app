60d47353c92bfc319fddad06f0cfdcaf256c18ae
studentchetan <chetankar65@gmail.com>
2018-02-20 16:55:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,15 @@ var img = document.getElementById('madi');
 madi.onclick = function() {
   madi.style.marginleft = '100px';  
 };
+
+
+//counter Javascript
+var button = document.getElementById('counter');
+button.onclick = function () {
+    //Make a request t the counter endpoint and capture a response,store it in a avariable,render the variable in the correct span.
+    counter = counter + 1;
+    var span = document.getElementById('count')
+    span.innerHTML = counter.toString();
+    
+    
+};
