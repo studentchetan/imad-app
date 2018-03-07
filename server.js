6422aba8775aa05b817de5c5063fb01e2ac71483
studentchetan <chetankar65@gmail.com>
2018-03-07 17:10:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config ={
     user: 'chetankar65',
@@ -33,6 +34,15 @@ app.get('/article-two', function(req,res){
 app.get('/article-three', function(req,res){
       res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 
+});
+
+function hash(input, salt) {
+    var hashed = crypto.pbkdf2Sync(input , salt ,10000 ,512 ,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req,res){
+    var hashedString = hash(req.params.input, 'This-Is-Some-Random-String');
+    res.send(hashedString);
 });
 
 app.get('/ui/style.css', function (req, res) {
