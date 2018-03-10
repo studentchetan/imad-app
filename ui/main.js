85018f556e0bbbdb0a584c20e000f7bb0373e768
studentchetan <chetankar65@gmail.com>
2018-03-10 09:31:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 //move the image
 
-//counter Javascript
+//Make new XML Http Request over here and send it to the Register endpoint.
+//We'll make the login form in the Home page itself
 
 
