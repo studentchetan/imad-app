7b9bb89b6087e49f02d8791b1e8e721b22db3ad5
studentchetan <chetankar65@gmail.com>
2018-03-13 11:27:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,7 @@ console.log('Loaded!');
 //Make new XML Http Request over here and send it to the Register endpoint.
 //We'll make the login form in the Home page itself
 
+//First,we'll design the form on the register.html and make it look good.
+
+
 
